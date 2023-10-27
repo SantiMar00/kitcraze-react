973d75c2559161af47c1fa03975611e60eb6c66c
Santiago Mariño <santimate30@hotmail.com>
2023-10-27 15:07:31 -0300

    no se

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,44 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import './Header.css'
 import Logo from '../../assets/logo.png'
 import Cart from '../cart/Cart'
 
 function Header() {
     const [isCartShown, setIsCartShown] = useState(false)
+    const [isMounted, setIsMounted] = useState(false)
+
+    const mountedStyle = {
+        animation: 'inAnimation 0.4s ease-in',
+    }
+    const unmountedStyle = {
+        animation: 'outAnimation 0.4s ease-out',
+        animationFillMode: 'forwards',
+    }
+
+    const mountedOverlayStyle = {
+        animation: 'inOverlayAnimation 0.3s ease-in',
+    }
+    const unmountedOverlayStyle = {
+        animation: 'outOverlayAnimation 0.3s ease-out',
+        animationFillMode: 'forwards',
+    }
+
+    function useOutsideAlerter(ref) {
+        useEffect(() => {
+            function handleClickOutside(event) {
+                if (ref.current && !ref.current.contains(event.target)) {
+                    setIsMounted(false)
+                }
+            }
+            document.addEventListener('mousedown', handleClickOutside)
+            return () => {
+                document.removeEventListener('mousedown', handleClickOutside)
+            }
+        }, [ref])
+    }
+
+    const wrapperRef = useRef(null)
+    useOutsideAlerter(wrapperRef)
 
     return (
         <div class="header">
@@ -32,8 +66,8 @@ function Header() {
                         </a>
                     </li>
                     <li>
-                        <a href="/clearance" class="li-a">
-                            NEWLY ADDED
+                        <a href="/national-teams" class="li-a">
+                            NATIONAL TEAMS
                         </a>
                     </li>
                     <li>
@@ -49,9 +83,8 @@ function Header() {
                         <a
                             style={{ cursor: 'pointer' }}
                             onClick={() => {
-                                isCartShown
-                                    ? setIsCartShown(false)
-                                    : setIsCartShown(true)
+                                setIsMounted(!isMounted)
+                                if (!isCartShown) setIsCartShown(true)
                             }}
                         >
                             <svg
@@ -62,17 +95,32 @@ function Header() {
                                 <path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z" />
                             </svg>
                         </a>
-                        {isCartShown && (
-                            <div class="cart-wrapper">
-                                <Cart />
-                            </div>
-                        )}
                     </li>
                     <li>
                         <p>-</p>
                     </li>
                 </ul>
             </div>
+            {isCartShown && (
+                <div
+                    ref={wrapperRef}
+                    class="cart-wrapper"
+                    style={isMounted ? mountedStyle : unmountedStyle}
+                    onAnimationEnd={() => {
+                        if (!isMounted) setIsCartShown(false)
+                    }}
+                >
+                    <Cart />
+                </div>
+            )}
+            {isCartShown && (
+                <div
+                    class="overlay"
+                    style={
+                        isMounted ? mountedOverlayStyle : unmountedOverlayStyle
+                    }
+                />
+            )}
         </div>
     )
 }
